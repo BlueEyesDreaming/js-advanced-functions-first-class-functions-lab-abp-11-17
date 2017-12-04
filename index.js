19c9eba1f916cf1396d9812c53acf5db0f7b1741
Ashton Reed <dreamergirl411@gmail.com>
2017-12-04 02:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const fareTripler = createFareMultiplier(3)
 
 
 
-const selectDifferentDrivers = function(arrayOfDrivers, funct) {
+const selectDifferentDrivers = function(drivers, funct) {
   console.log(arrayOfDrivers)
   console.log(funct)
   if (funct === returnFirstTwoDrivers()) {
