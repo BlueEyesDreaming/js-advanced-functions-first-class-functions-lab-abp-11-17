9339b2d9490ae504715d872cc92bd06a345af46e
Ashton Reed <dreamergirl411@gmail.com>
2017-12-04 02:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
 
+
 const selectDifferentDrivers = function(arrayOfDrivers, funct) {
   if (funct === returnFirstTwoDrivers() {
     return returnFirstTwoDrivers()
   } else if (funct === returnLastTwoDrivers() {
     return returnLastTwoDrivers()
   }
-  return function(drivers)
 }
